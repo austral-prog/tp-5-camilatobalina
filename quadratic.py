be10ff3ac797b67a6a4462012990a52c9a461e14
Camila Tobalina <162723051+camilatobalina@users.noreply.github.com>
2024-04-22 13:00:20 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,27 +1,27 @@
     import math
     def roots(a, b, c):
-        var1 = b * 2 - 4 * a * c
+        var1 = b ** 2 - 4 * a * c
         if var1 < 0:
-            return "()"
+            return "( )"
         
         raiz= math.sqrt(var1)
         result1= (-b+raiz) / (2 * a)
         result2 = (-b - raiz) / (2 * a)
-        if result1 != result2:
-            return f"{result1}"
+        if result1 == result2:
+            return f"({result1})"
         else:
-            return f"{result1},{result2}"
+            return f"({result1},{result2})"
     def value_y(a, b, c, x):
         imagen = a * (x ** 2) + b * x + c
         return imagen
 
     def to_string(a, b, c):
         if a and b and c:
-            return f'{a}* X^2 +{b}*X+{c}'
+            return f"f{a} * X^2 +{b}*X+{c}"
         elif b and c:
-            return f"f(x) = {b}+ {c}"
+            return f"f(x) = {b} * X + {c}"
         elif a and c:
-            return f"f(x) = {a} + X^2 + {c}"
+            return f"f(x) = {a} * X^2 + {c}"
         elif not a and not b and c:
             return f"f(x) = {c}"
         
