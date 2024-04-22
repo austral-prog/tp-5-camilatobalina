7a78c2c57858bf20b87f5952e55bb75749f56204
Camila Tobalina <162723051+camilatobalina@users.noreply.github.com>
2024-04-22 14:25:15 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,23 +1,23 @@
-    import math
-    def roots(a, b, c):
-        var1 = b ** 2 - 4 * a * c
-        if var1 < 0:
-            return "( )"
+import math
+def roots(a, b, c):
+    var1 = b ** 2 - 4 * a * c
+    if var1 < 0:
+        return "( )"
         
-        raiz= math.sqrt(var1)
-        result1= (-b+raiz) / (2 * a)
-        result2 = (-b - raiz) / (2 * a)
-        if result1 == result2:
+        raiz = math.sqrt(var1)
+        result1 = (-b+raiz)/(2*a)
+        result2 = (-b-raiz)/(2*a)
+        if result1==result2:
             return f"({result1})"
         else:
             return f"({result1},{result2})"
     def value_y(a, b, c, x):
-        imagen = a * (x ** 2) + b * x + c
+        imagen = a*(x**2) + b*x + c
         return imagen
 
     def to_string(a, b, c):
         if a and b and c:
-            return f"f{a} * X^2 +{b}*X+{c}"
+            return f"f{a}*X^2 + {b}*X + {c}"
         elif b and c:
             return f"f(x) = {b} * X + {c}"
         elif a and c:
@@ -29,8 +29,8 @@
 
     def derivation(a, b):
         if a and b:
-            return f"f'(x) = {2 * a} * X + {b}"
+            return f"f'(x) = {2*a}*X + {b}"
         elif not a:
             return f"f'(x) = {b}"
         elif not b:
-            return f"f'(x) = {2 * a} * X"
+            return f"f'(x) = {2*a}*X"
