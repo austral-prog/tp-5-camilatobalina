a74a00426cc848f5a79b8225e5b1c5fc258ac4ac
Camila Tobalina <162723051+camilatobalina@users.noreply.github.com>
2024-04-22 22:22:24 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,8 +1,8 @@
 import math
-def roots(a, b, c):
+def roots(a,b,c):
     var1 = b**2-4*a*c
     if var1 < 0:
-        return "()"
+        return "( )"
         
     raiz = math.sqrt(var1)
     result1 = (-b+raiz)/2*a
